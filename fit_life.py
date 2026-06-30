f32e2679aa1c8e980d56e19cbb96eb5300fdd489
Amina Dzhabrailova <amina29.11@mail.ru>
2026-06-30 23:23:31 +0300

    текст комментария

--- fit_life.py
+++ fit_life.py
@@ -1,31 +1,17 @@
 WATER_PER_KG = 30  # константа
-MILLILITERS_PER_LITER = 1000  # константа
+MILLILITERS_PER_LITER = 100  # константа
 
-user_name = input("Здравствуйте, введите, пожалуйста, ваше имя: ").strip()
-while True:
-    try:
-        user_age = int(input("Введите, пожалуйста, ваш возраст: "))
-        break
-    except ValueError:
-        print("Ох! Думаю вы не туда нажали. Попробуйте еще раз:")
-while True:
-    try:
-        user_weight = int(input("Введите, пожалуйста, ваш вес(в кг): "))
-        break
-    except ValueError:
-        print("Ох! Думаю вы не туда нажали. Попробуйте еще раз:")
-while True:
-    try:
-        user_height = float(input("Введите ваш рост, например, 2.00: "))
-        break  # избавились от "пожалуйста", тк не умещались в кол-во символов
-    except ValueError:
-        print("Ох! Думаю вы не туда нажали. Введите еще раз, используйте '.':")
-bmi = round((user_weight / (user_height ** 2)), 1)  # расчёт индекса массы тела
+user_name = input("Здравствуйте, введите, пожалуйста, ваше имя: ")
+user_age = int(input("Введите, пожалуйста, ваш возраст: "))
+user_weight = float(input("Введите, пожалуйста, ваш вес(в кг): "))
+user_height = float(input("Введите, пожалуйста, ваш рост(например, 2.00): "))
+bmi = user_weight / (user_height ** 2)  # расчёт индекса массы тела
+bmi = round(bmi, 1)  # округление ИМТ до одного знака после запятой
 water_ml = user_weight * WATER_PER_KG  # норма воды в миллилитрах
 water_l = water_ml / MILLILITERS_PER_LITER  # переводим в литры норму воды
 
 print(f"\nОтчет для пользователя: {user_name} ({user_age} г.)")
 print(f"Ваш Индекс Массы Тела: {bmi}")
 print(f"Рекомендуемая норма воды: {water_l:.1f} л. в день")
-print("-" * 30)
+print()
 print("Расчет окончен. Будьте здоровы!")
