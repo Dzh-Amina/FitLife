fca2d6a63298a689534ab81b9767740349b4041d
Amina Dzhabrailova <amina29.11@mail.ru>
2026-07-01 21:09:37 +0300

    текст комментария

--- fit_life.py
+++ fit_life.py
@@ -2,17 +2,18 @@ WATER_PER_KG = 30  # константа
 MILLILITERS_PER_LITER = 1000  # константа
 
 
-def bmi(user_weight, user_height):  # расчёт индекса массы тела
-    bmi = round((user_weight / (user_height ** 2)), 1)
-    return bmi
+def bmi(user_weight, user_height):
+    """расчёт индекса массы тела"""
+    return round((user_weight / (user_height ** 2)), 1)
 
 
-def water_l(user_weight):  # расчет нормы воды
-    water_littrs = user_weight * WATER_PER_KG / MILLILITERS_PER_LITER
-    return water_littrs
+def water_l(user_weight):
+    """расчет нормы воды"""
+    return user_weight * WATER_PER_KG / MILLILITERS_PER_LITER
 
 
 user_name = input("Введите, пожалуйста, ваше имя: ")
+
 while True:
     try:
         user_age = int(input("Введите, пожалуйста, ваш возраст: "))
